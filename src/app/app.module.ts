77688c28f6b047d0682cb564fa145de912b85cfe
safanamunir <safanamunir64@gmail.com>
2021-07-06 22:03:05 +0100

    add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArchiveComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+       {path: '', component: HomeComponent},
+       {path: 'archive/:year/:month', component: ArchiveComponent},
+       {path: '**', component: NotFoundComponent}
+
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
